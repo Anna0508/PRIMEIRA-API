6abab93a15dd4ef4613e707889f96198617ae0ef
annaluisaalexandre888@gmail.com <annaluisaalexandre888@gmail.com>
2026-08-12 16:53:15 -0300

    alt.main

--- main.py
+++ main.py
@@ -4,38 +4,31 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from config import logger
 from database import USUARIOS_DB
-from fastapi import Depends, HTTPException, status
-from auth import obter_usuario_atual, verificar_permissao_admin
+from auth import obter_usuario_atual, verificar_senha, criar_token
 
-
-app = FastAPI(title="API segura")
+app= FastAPI()
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     usuario = USUARIOS_DB.get(form_data.username)
-    if not usuario or usuario["password"] != form_data.password:
+
+    if not usuario:
+        logger.warning(f"Falha no login:{form_data.username}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Usuário invalido",
-            headers={"WWW-Authenticate": "Bearer"},
-
+            detail="Usuario ou senha incorreto",
+            headers={"WWWA-Authenticate": "Bearer"},
         )
-    logger.info(f"Usuário logado: {form_data.username}")
-
-    return {"access_token": form_data.username, "token_type": "bearer"}
-
-@app.get("/admin/painel")
-async def painel_admin(usuario: dict = Depends(obter_usuario_atual)):
-    print(usuario)
-    logger.info(f"Usuário com permissãoo de admin acessou o painel_admin")
-    return {"mensaagem": "Bem-vindo ao painel de admin"}
-
-
-@app.get("/users/{user_id}")
-def get_user(user_id: str, usuario: dict = Depends(obter_usuario_atual)):
-    logger.info(f"Usuário {usuario} acessou o endpoint /users/{user_id}")
-
-    return {"user_id": user_id, "usuario": usuario} 
+    if not verificar_senha(form_data.password, usuario["password"]):
+        logger.warning(f"Falha de login:{form_data.username}")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="usuario ou senha incorretos",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    token_real=criar_token(data={"sub": form_data.username})
+    logger.info(f"Usuario logado com sucesso:{form_data.username}")
+    return{"access_token":token_real,"token_type":"bearer"}
 
    
               
