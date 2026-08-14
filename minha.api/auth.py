0ddbcb60b74d91cae273964aa6ab4873431b1ebd
Anna Luisa <annaluisaalexandre888@gmail.com>
2026-08-14 16:56:42 -0300

    Add files via upload

---
+++ minha.api/auth.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timedelta, timezone
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer 
+from pwdlib import PasswordHash
+from database import USUARIOS_DB
+from config import logger
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+if not SECRET_KEY:
+        raise ValueError("A SECRET_KEY não foi encontrada no arquivo .env")
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTE = 30
+
+pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+
+def verificar_senha(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
+
+def criar_token(data:dict):
+        dados_copia = data.copy()
+        tempo_expiracao = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTE)
+        dados_copia.update({"exp": tempo_expiracao})
+
+        token_jwt = jwt.encode(dados_copia, SECRET_KEY, algorithm=ALGORITHM)
+        return token_jwt
+
+def obter_usuario_atual(token:str = Depends(oauth2_scheme)):
+        try:
+                payload = jwt.decode(token,SECRET_KEY, algorithms=[ALGORITHM])
+                email: str = payload.get("sub")
+
+                if email is None:
+                        raise HTTPException(
+                                status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="token invalido",
+                        )
+                usuario = USUARIOS_DB.get(email)
+                if not usuario:
+                        raise HTTPException(
+                                status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="token invalido"
+                        )
+                
+                return usuario 
+        
+        except jwt.PyJWTError:
+                raise HTTPException(
+                        status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail="token invalido",
+                )
+        
+
+                
+                
+
+        
+    
+    
+        
+
+   
+
+
+             
+    
